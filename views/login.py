506d1a5c349d5d216b9ac8c9942b9b1ec66074b3
dkrivaa <dannykrivaa@hotmail.com>
2025-04-22 15:32:21 +0300

    Initial commit

--- views/login.py
+++ views/login.py
@@ -7,11 +7,12 @@ def main():
     """ This function is for simple login """
     load_dotenv()
     # Login
-    code = st.text_input('Enter code', type='password')
-    if code == os.getenv('CODE'):
-        st.switch_page('views/home.py')
-    else:
-        st.write('Incorrect Code')
+    code = st.text_input('Enter code', type='password', value=None)
+    if code is not None:
+        if code == os.getenv('CODE'):
+            st.switch_page('views/home.py')
+        else:
+            st.write('Incorrect Code')
 
 
 if __name__ == '__main__':
